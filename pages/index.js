2ae645f1a9f816009a4886cd37cf2cd23a1ab94a
Dung Truong <checkraiser11@gmail.com>
2018-12-27 18:53:30 +0700

    Add redux

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
 import {Button} from 'antd'
 import withStore from 'next-app-store/lib/with-store'
+import {connect} from 'react-redux'
 
-const Page = () => (
-  <div><Button>Hello</Button></div>
+const Page = ({hello, changeData}) => (
+  <div><Button onClick={changeData}>{hello}</Button></div>
 )
-export default withStore(Page)
+
+const EnhancedPage = connect(
+  state => ({ hello: state.test.data }),
+  dispatch => ({ changeData: dispatch.test.changeText })
+)(Page)
+EnhancedPage.getInitialConfig = async (ctx) => {
+  return {
+    redux: 'REDUX_STORE_INDEX'
+  }
+}
+EnhancedPage.getInitialStore = (ctx) => {
+  return {
+    test: {
+      data: 'Hello World 1',
+      changeText: (state, payload) => {
+        state.data = 'Leuleu'
+      }
+    }
+  }
+}
+
+export default withStore(EnhancedPage)
 
